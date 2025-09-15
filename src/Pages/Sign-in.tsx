254c7c02f293601eb6c58546f7ee656753455d83
gustamara <gustacamra.dev@hotmail.com>
2025-09-15 02:19:35 -0300

    implements signin backend

--- src/Pages/Sign-in.tsx
+++ src/Pages/Sign-in.tsx
@@ -1,3 +1,4 @@
+import { authenticateUser, registerUser } from "@/api/user"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -5,14 +6,13 @@ import { useForm } from "react-hook-form"
 import { Link, useNavigate, useSearchParams } from "react-router-dom"
 
 interface SignInForm {
-  email: String
-  password: String
+  email: string
+  password: string
 }
 
 export function SignIn() {
   const navegate = useNavigate()
   const [searchParams] = useSearchParams()
-
 
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>({
     defaultValues: {
@@ -20,51 +20,61 @@ export function SignIn() {
     }
   })
 
-  const submitSignInForm = (data: SignInForm) => {
-    navegate("/", {state: data})
+  async function authUser(data: SignInForm) {
+    const result = await authenticateUser(data)
+    return result
   }
 
-  return (
-    <>
-      <div className="p-8">
-        <Button asChild className="absolute top-8 right-8 border-1" variant="ghost">
-          <Link to="../sign-up">Faça seu login</Link>
-        </Button>
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
-            </h1>
-            <p className="text-muted-foreground text-sm">
-              Visualize todos os seus dados aqui
-            </p>
+  const submitSignInForm = async (data: SignInForm) => {
+    const result = await authUser(data)
+    if (!result) {
+      alert("E-mail ou senha incorretos.")
+    } else {
+      navegate("/")
+    }
+  }
+
+return (
+  <>
+    <div className="p-8">
+      <Button asChild className="absolute top-8 right-8 border-1" variant="ghost">
+        <Link to="../sign-up">Faça seu login</Link>
+      </Button>
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Visualize todos os seus dados aqui
+          </p>
+        </div>
+
+        <form className="space-y-4" onSubmit={handleSubmit(submitSignInForm)}>
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input
+              id="email"
+              type="email"
+              {...register('email')}
+            />
+
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Sua senha</Label>
+            <Input
+              id="password"
+              type="password"
+              {...register('password')}
+            />
           </div>
 
-          <form className="space-y-4" onSubmit={handleSubmit(submitSignInForm)}>
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input
-                id="email"
-                type="email"
-                {...register('email')}
-              />
-
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Sua senha</Label>
-              <Input
-                id="password"
-                type="password"
-                {...register('password')}
-              />
-            </div>
-
-            <Button className="w-full" type="submit" disabled={isSubmitting}>
-              Acessar Painel
-            </Button >
-          </form>
-        </div>
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            Acessar Painel
+          </Button >
+        </form>
       </div>
-    </>
-  )
+    </div>
+  </>
+)
 }
